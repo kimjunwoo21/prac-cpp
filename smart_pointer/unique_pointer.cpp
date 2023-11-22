d78a1975e10d6e33af6ab5d7ca06bedf9ae1a087
Junwoo Kim <kimjunwoo@hyunbo.com>
2023-11-22 14:42:30 +0900

    add carList

--- smart_pointer/unique_pointer.cpp
+++ smart_pointer/unique_pointer.cpp
@@ -7,13 +7,23 @@ Car::Car(std::string name)
 {
     mPos->reserve(2); // the unique pointer mPos don't need destructor
 
+    mPos->push_back(0.0f);
+    mPos->push_back(0.0f);
+    
     std::cout << name << std::endl;
 }
 
 
 int main(void)
 {
-    Car car("my car");
+    std::vector<std::unique_ptr<Car> > carList;
+
+    carList.reserve(2);
+
+    carList.push_back(std::unique_ptr<Car>(new Car("first car")));
+    carList.push_back(std::unique_ptr<Car>(new Car("second car")));
     
+    carList.clear(); // call first and second car destructor that the Car class don't have. but it's okay.
+
     return 0;
 }
